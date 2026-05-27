81452d17f89ff75bf18bd3e721ab5ca8e608cd28
naman100x <namanagota369@gmail.com>
2026-05-28 02:36:42 +0530

    login

--- lib/core/constants/ApiUrlConstants.dart
+++ lib/core/constants/ApiUrlConstants.dart
@@ -1,7 +1,7 @@
 class ApiUrlConstants {
   static String BASE_URL = "";
-  //static String UAT_BASE_URL = "https://arham-jewellers-backend.onrender.com";
-  static String UAT_BASE_URL = "https://bed4-103-58-152-137.ngrok-free.app";
+  static String UAT_BASE_URL = "https://arham-jewellers-backend.onrender.com";
+  //static String UAT_BASE_URL = "https://3be3-103-58-152-137.ngrok-free.app";
   static String WEBSITE_URL = "";
 
   //static String UAT_BASE_URL = "http://localhost:5000";
