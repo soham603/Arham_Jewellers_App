5fd765489b41659a8711761249cecc4460833ed6
Parth Baviskar <parth.baviskar.ms@outlook.com>
2026-06-08 04:05:23 +0530

    refactor: simplify ColorPalette and remove dark mode support
    - Remove isDarkMode parameter from ColorPalette, hardcode light mode colors
    - Delete TextThemeDecoration class (unused after palette simplification)
    - Simplify ContextExtensions: remove TextThemeDecoration getter, maxContentWidth, platform helpers

--- lib/utils/ColorPallete.dart
+++ lib/utils/ColorPallete.dart
@@ -1,56 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ColorPalette {
-  final bool isDarkMode;
+  Color get backgroundColor => const Color(0xFFFFFFFF);
 
-  ColorPalette(this.isDarkMode);
+  Color get primaryColor => const Color(0xFF8B6914);
 
-  Color get backgroundColor =>
-      isDarkMode ? const Color(0xFFFFFFFF) : const Color(0xFFFFFFFF);
+  Color get boxColor => const Color(0xFFFAF6F0);
 
-  Color get primaryColor =>
-      isDarkMode ? const Color(0xFF8B6914) : const Color(0xFF8B6914);
+  Color get buttonColor => const Color(0xff000000);
 
-  Color get boxColor =>
-      isDarkMode ? const Color(0xFFFAF6F0) : const Color(0xFFFAF6F0);
+  Color get textColor => const Color(0xff000000);
 
-  Color get onlyAccentColor => const Color(0xffFFFDD00);
+  Color get reverseTextColor => const Color(0xffFFFFFF);
 
-  Color get reversedAccentColor =>
-      isDarkMode ? const Color(0xff94368E) : const Color(0xfffffdd00);
+  Color get lightBackgroundColor => const Color(0xFF999999);
 
-  Color get buttonColor =>
-      isDarkMode ? const Color(0xff000000) : const Color(0xff000000);
+  Color get pageBackgroundColor => const Color(0xFFF6F6F6);
 
-  Color get textColor =>
-      isDarkMode ? const Color(0xff000000) : const Color(0xff000000);
-
-  Color get reverseTextColor =>
-      isDarkMode ? const Color(0xffFFFFFF) : const Color(0xffFFFFFF);
-
-  Color get lightBackgroundColor =>
-      isDarkMode ? const Color(0xFF999999) : const Color(0xFF999999);
-
-  Color get pageBackgroundColor =>
-      isDarkMode ? const Color(0xFFF6F6F6) : const Color(0xFFF6F6F6);
-
-  Color get subTitleColor =>
-      isDarkMode ? const Color(0xFF767C8F) : const Color(0xFF767C8F);
+  Color get subTitleColor => const Color(0xFF767C8F);
 
   Color get shimmerBaseColor => Colors.grey[300]!;
 
   Color get shimmerHighLightColor => Colors.grey[100]!;
-
-  Color get winnerColor => const Color(0xFFF8FFBF);
-
-  Color get referralColor =>
-      isDarkMode ? const Color(0xFFC6F224) : const Color(0xFFC6F224);
-
-  Color get likeColor =>
-      isDarkMode ? const Color(0xFFC6F224) : const Color(0xFFC6F224);
-
-  Color get secondaryColor =>
-      isDarkMode ? const Color(0xFFE1FC80) : const Color(0xFFE1FC80);
 
   Color get gold => const Color(0xFFD4AF37);
 
@@ -66,11 +37,7 @@ class ColorPalette {
 
   Color get cardBg => const Color(0xFFFFFFFF);
 
-  Color get chipBg => const Color(0xFFF0E8D0);
-
   Color get level3Bg => const Color(0xFFF9F4E8);
-
-  Color get productBg => const Color(0xFFFFFBF2);
 
   Color get k18Accent => const Color(0xFFFFD700);
 
