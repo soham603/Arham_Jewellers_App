f74db6911dab73f31bbf4bf2d816d7b9e347524c
Parth Baviskar <parth.baviskar.ms@outlook.com>
2026-06-08 04:05:23 +0530

    refactor: simplify ColorPalette and remove dark mode support
    - Remove isDarkMode parameter from ColorPalette, hardcode light mode colors
    - Delete TextThemeDecoration class (unused after palette simplification)
    - Simplify ContextExtensions: remove TextThemeDecoration getter, maxContentWidth, platform helpers

--- lib/utils/ContextExtensions.dart
+++ lib/utils/ContextExtensions.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ratnesh_gold_app/utils/ColorPallete.dart';
-import 'package:ratnesh_gold_app/utils/TextThemeDecoration.dart';
 
 extension AppContextExtensions on BuildContext {
-  ColorPalette get colorPalette => ColorPalette(Theme.of(this).brightness == Brightness.dark);
-
-  TextThemeDecoration get textThemeDecoration =>
-      TextThemeDecoration(Theme.of(this).brightness == Brightness.dark, this);
+  ColorPalette get colorPalette => ColorPalette();
 
   double getScreenWidth(double percentage) => MediaQuery.of(this).size.width * (percentage / 100);
 
   double getScreenHeight(double percentage) => MediaQuery.of(this).size.height * (percentage / 100);
-
-  double get maxContentWidth => 600.0;
 
   double responsiveWidth(double phoneVal, {double? tabletVal}) =>
       _isTablet ? (tabletVal ?? phoneVal * 1.15) : phoneVal;
@@ -24,11 +18,4 @@ extension AppContextExtensions on BuildContext {
       _isTablet ? tablet : phone;
 
   bool get _isTablet => MediaQuery.of(this).size.width >= 600;
-
-  TargetPlatform get platform => Theme.of(this).platform;
-  bool get isIOS => platform == TargetPlatform.iOS;
-  bool get isAndroid => platform == TargetPlatform.android;
-  bool get isMacOS => platform == TargetPlatform.macOS;
-  bool get isWindows => platform == TargetPlatform.windows;
-  bool get isWeb => platform == TargetPlatform.fuchsia;
 }
